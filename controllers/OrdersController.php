f046e0c97d73ba11e06fb0dd3c05db695496d967
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-03 19:51:25 +0100

    delete order

--- controllers/OrdersController.php
+++ controllers/OrdersController.php
@@ -8,7 +8,7 @@ class OrdersController
         return $orders;
     }
 
-    
+
     public function addOrder($data)
     {
         $result = Order::createOrder($data);
@@ -24,6 +24,39 @@ class OrdersController
             Redirect::to("home");
         }
     }
+
+    public function removeOrder()
+    {
+        if (isset($_POST["delete_order_id"])) {
+            $data = array(
+                "id" => $_POST["delete_order_id"]
+            );
+            $result = Order::deleteOrder($data);
+            if ($result === "ok") {
+                Session::set("success", "Order supprimé");
+                Redirect::to("orders");
+            } else {
+                echo $result;
+            }
+        }
+    }
+
+    public function valider()
+    {
+        if (isset($_POST["validateOrderId"])) {
+            $data = array(
+                "id" => $_POST["validateOrderId"]
+            );
+            $result = Order::deleteOrder($data);
+            if ($result === "ok") {
+                Session::set("success", "Order supprimé");
+                echo '<script type="text/javascript">document.getElementById("order_' . $_POST["validateOrderId"] . '").style.backgroundColor = "green";</script>';
+                Redirect::to("orders");
+            } else {
+                echo $result;
+            }
+        }
+    }
+
+    
 }
-
-
