023d896127b25854f6c9768ebb05f58ce199237b
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-03 19:56:18 +0100

    page about

--- views/about.php
+++ views/about.php
@@ -1,14 +1,14 @@
 <div class="container">
-    <h1 class="text-center">À propos de notre boutique en ligne</h1>
-    <div class="row mt-5">
-        <div class="col-md-6">
-            <p style="font-size: 18px;line-height: 1.5;">Notre boutique en ligne est un distributeur de produits électroniques de qualité supérieure. Nous proposons une large sélection de produits, allant des téléphones portables aux ordinateurs en passant par les appareils électroménagers. Nous avons une équipe dévouée qui travaille sans relâche pour vous offrir les meilleurs produits et un service client de qualité.</p>
-            <p>Nous nous efforçons de maintenir des prix compétitifs tout en offrant des produits de qualité supérieure. Nous avons des relations étroites avec les principaux fabricants et fournisseurs, ce qui nous permet de vous offrir les derniers produits technologiques à des prix raisonnables.</p>
-            <p>Notre site est conçu pour une expérience d'achat en ligne facile et sécurisée. Nous avons une politique de retour facile et nous nous engageons à résoudre tous les problèmes de manière rapide et efficace.</p>
-            <p>Nous espérons que vous apprécierez votre expérience d'achat sur notre site et nous nous réjouissons de vous servir à l'avenir.</p>
-        </div>
-        <div class="col-md-6">
-            <img width="600px" src="<?php echo BASE_URL; ?>public/images/OIP.jpeg" alt="Un produit électronique">
-        </div>
+  <h1 class="text-center mt-5">À propos de notre boutique en ligne</h1>
+  <div class="row mt-5">
+    <div class="col-md-6">
+      <p class="font-weight-light font-size-18 line-height-1-5">Notre boutique en ligne est un distributeur de produits électroniques de qualité supérieure. Nous proposons une large sélection de produits, allant des téléphones portables aux ordinateurs en passant par les appareils électroménagers. Nous avons une équipe dévouée qui travaille sans relâche pour vous offrir les meilleurs produits et un service client de qualité.</p>
+      <p class="font-weight-light">Nous nous efforçons de maintenir des prix compétitifs tout en offrant des produits de qualité supérieure. Nous avons des relations étroites avec les principaux fabricants et fournisseurs, ce qui nous permet de vous offrir les derniers produits technologiques à des prix raisonnables.</p>
+      <p class="font-weight-light">Notre site est conçu pour une expérience d'achat en ligne facile et sécurisée. Nous avons une politique de retour facile et nous nous engageons à résoudre tous les problèmes de manière rapide et efficace.</p>
+      <p class="font-weight-light">Nous espérons que vous apprécierez votre expérience d'achat sur notre site et nous nous réjouissons de vous servir à l'avenir.</p>
     </div>
+    <div class="col-md-6">
+      <img width="100%" class="img-fluid" src="<?php echo BASE_URL; ?>public/images/OIP.jpeg" alt="Un produit électronique">
+    </div>
+  </div>
 </div>
