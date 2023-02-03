11ba6e727716c27047fb111fbe28c22585d7c982
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-03 19:58:36 +0100

    add lien script

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>/public/css/style.css">
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
     <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
-    
+
 </head>
 
 <body>
@@ -20,14 +20,20 @@
                 <li class="list-inline-item"><a href="#">Accueil</a></li>
                 <li class="list-inline-item"><a href="#">Produits</a></li>
                 <li class="list-inline-item"><a href="#">Contacts</a></li>
-               
+
             </ul>
             <p class="copyright">Jelouani@Youcode © <?php echo date("Y"); ?> Copyright.</p>
         </footer>
     </div>
-   <script src="<?php echo BASE_URL; ?>/public/js/main.js"></script>
+    <script src="<?php echo BASE_URL; ?>/public/js/main.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.2/gsap.min.js"></script>
+    <!-- CSSRulePlugin JS -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/3.3.2/CSSRulePlugin.min.js"></script>
+    <!-- Animated Slider JS -->
+    <script src="js/animated-slider.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
 
 </body>
+
 </html>
