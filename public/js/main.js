e8328c185bfed185ef1f2314264e664e25ba283b
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-02-03 19:55:59 +0100

    valider et supprimer order code js

--- public/js/main.js
+++ public/js/main.js
@@ -11,7 +11,6 @@ function submitForm($id) {
     const form = document.querySelector("#form");
     input.value = $id;
     form.submit();
-    // console.log(12);
 }
 
 
@@ -20,7 +19,6 @@ function submitFormCat($id) {
     const form = document.querySelector("#form");
     input.value = $id;
     form.submit();
-    // console.log(12);
 }
 
 
@@ -32,61 +30,139 @@ function deleteFormCat($id) {
 }
 
 
-function formAchat() {
-    const form = document.querySelector("#achat");
-    form.innerHTML = `
-    <form class="" style="width: 600px;border-style: solid;padding:2rem;justify-content:center;justify-items: center;">
-        <div class="form-group mb-3">
-            <label for="card-number" class="form-label">Card Number</label>
-            <input type="text" class="form-control" id="card-number" aria-describedby="emailHelp">
-        </div>
-        <div class="form-group mb-3">
-            <label for="card-holder-name" class="form-label">Cardholder name</label>
-            <input type="text" class="form-control" id="card-holder-name">
-        </div>
-        <div class="form-group mb-3">
-            <label for="expiry-date">Expiry date</label>
-            <input class="form-control expire" type="text" placeholder="MM / YYYY" id="expiry-date" />
-        </div>
-        <div class="form-group mb-3">
-            <label for="security-number">Security Number</label>
-            <input class="form-control ccv" type="text" placeholder="CVC" maxlength="3" id="security-number"/>
-        </div>
-        <button class="btn btn-primary buy" type="submit" id="paypalbutton"><i class="fa fa-cc-paypal fa-brands"></i> Pay€</button>
-    </form>`;
-    document.querySelector("#paypalbutton").addEventListener("click", alertCommand);
-}
-
-
-document.querySelector("#buy-button").addEventListener("click", formAchat);
-
-
-function alertCommand() {
-    alert("Votre commande éffectue (❤️)");
+function deleteOrder($id) {
+    const input = document.querySelector("#delete_order_id");
+    const form = document.querySelector("#delete_order");
+    input.value = $id;
+    form.submit();
 }
 
 
 
 
+function Valider(orderId) {
+  let row = document.querySelector(`tr[data-order-id="${orderId}"]`);
+  
+  // Vérifier si l'ID de la commande a déjà été stocké dans localStorage
+  if (localStorage.getItem(`orderId_${orderId}`) === 'validated') {
+    // Si oui, définir la couleur enregistrée sur la ligne
+    row.style.backgroundColor = "green";
+  } else {
+    // Sinon, définir la couleur de la ligne en vert
+    row.style.backgroundColor = "green";
+    // Et enregistrer l'ID de la commande dans localStorage
+    localStorage.setItem(`orderId_${orderId}`, 'validated');
+  }
+}
 
-// function updateTotal(price, qtyId, totalId) {
-//     var qty = document.getElementById(qtyId).value;
-//     var total = price * qty;
-//     document.getElementById(totalId).innerHTML = total;
+document.addEventListener("DOMContentLoaded", function() {
+  // Récupérer toutes les lignes de commandes
+  let rows = document.querySelectorAll("tr[data-order-id]");
+
+  // Boucler sur chaque ligne
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    let orderId = row.getAttribute("data-order-id");
+
+    // Vérifier si l'ID de la commande a déjà été stocké dans localStorage
+    if (localStorage.getItem(`orderId_${orderId}`) === 'validated') {
+      // Si oui, définir la couleur enregistrée sur la ligne
+      row.style.backgroundColor = "green";
+    }
+  }
+});
+
+
+
+
+function Delivery(orderId) {
+  let row = document.querySelector(`tr[data-order-id="${orderId}"]`);
+  
+  // Vérifier si l'ID de la commande a déjà été stocké dans localStorage
+  if (localStorage.getItem(`orderId_${orderId}`) === 'delivery') {
+    // Si oui, définir la couleur enregistrée sur la ligne
+    row.style.backgroundColor = "skyblue";
+  } else {
+    // Sinon, définir la couleur de la ligne en vert
+    row.style.backgroundColor = "skyblue";
+    // Et enregistrer l'ID de la commande dans localStorage
+    localStorage.setItem(`orderId_${orderId}`, 'delivery');
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  // Récupérer toutes les lignes de commandes
+  let rows = document.querySelectorAll("tr[data-order-id]");
+
+  // Boucler sur chaque ligne
+  for (let i = 0; i < rows.length; i++) {
+    let row = rows[i];
+    let orderId = row.getAttribute("data-order-id");
+
+    // Vérifier si l'ID de la commande a déjà été stocké dans localStorage
+    if (localStorage.getItem(`orderId_${orderId}`) === 'delivery') {
+      // Si oui, définir la couleur enregistrée sur la ligne
+      row.style.backgroundColor = "skyblue";
+    }
+  }
+});
+  
+
+
+
+
+
+
+
+
+
+
+// function formAchat() {
+//     const form = document.querySelector("#achat");
+//     form.innerHTML = `
+//     <form id="achat" style="width: 600px;border-style: solid;padding:2rem;justify-content:center;justify-items: center;">
+//         <div class="form-group mb-3">
+//             <label for="card-number" class="form-label">Card Number</label>
+//             <input type="text" class="form-control" id="card-number" aria-describedby="emailHelp" required>
+//         </div>
+//         <div class="form-group mb-3">
+//             <label for="card-holder-name" class="form-label">Cardholder name</label>
+//             <input type="text" class="form-control" id="card-holder-name" required>
+//         </div>
+//         <div class="form-group mb-3">
+//             <label for="expiry-date">Expiry date</label>
+//             <input class="form-control expire" type="text" placeholder="MM / YYYY" id="expiry-date" required />
+//         </div>
+//         <div class="form-group mb-3">
+//             <label for="security-number">Security Number</label>
+//             <input class="form-control ccv" type="text" placeholder="CVC" maxlength="3" id="security-number" required/>
+//         </div>
+//         <button class="btn btn-primary buy" type="submit" id="paypalbutton"><i class="fa fa-cc-paypal fa-brands"></i> Pay€</button>
+//     </form>`;
+
+//     document.querySelector("#paypalbutton").addEventListener("click", alertCommand);
+
 // }
 
 
-//   let amount = document.querySelector('#amount').dataset.amount;
-//   let finalAmount = Math.floor(amount / 9.86);
-//   Buttons({
-    
-//     onApprove: function(data, actions) {
-//       // This function captures the funds from the transaction.
-//       return actions.order.capture().then(function(details) {
-//         // This function shows a transaction success message to your buyer.
-//         alert('Commande effectuée par ' + details.payer.name.given_name);
-//         document.querySelector('#addOrder').submit();
-//       });
-//     }
-//   }).render('#paypal-button-container');
-  //This function displays Smart Payment Buttons on your web page.
+// document.querySelector("#buy-button").addEventListener("click", formAchat);
+
+
+
+// function alertCommand() {
+//     alert("Votre commande éffectue (❤️)");
+// }
+
+
+document.querySelector("#buy-button").addEventListener("click", function () {
+    document.getElementById("achat").style.display = "flex";
+});
+
+document.querySelector("#paypalbutton").addEventListener("click", function () {
+    var form = document.querySelector("#addorder");
+    if (form) {
+        form.submit();
+    }
+});
+
+
